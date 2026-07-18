2c4fe3bad654764b9b03ded7a9cc70edd149af39
爱玩电脑的乐乐 <118098992+aiwandiannaodelele@users.noreply.github.com>
2026-07-18 09:36:54 +0800

    fix: turbopack root warning

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   output: process.env.NODE_ENV === "production" ? "export" : undefined,
   images: { unoptimized: true },
   trailingSlash: true,
+  turbopack: {
+    root: process.cwd(),
+  },
 }
 
 export default nextConfig
